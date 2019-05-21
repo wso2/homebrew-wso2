cb40dfb5a116151d9c570e5d4757c9b15e26f6b4
irunika <irunikail@gmail.com>
2019-05-21 22:23:38 +0530

    Create formula for wso2is-5.8.0

---
+++ Formula/wso2is-5.8.0.rb
@@ -0,0 +1,24 @@
+class Wso2is580 < Formula
+  desc "WSO2 Identity Server 5.8.0"
+  homepage "https://wso2.com/api-management/"
+  url "https://dl.bintray.com/wso2/binary/wso2is-5.8.0.zip"
+  sha256 "3b6ac82f37f7b3ed8d92157cc516ea3212e369213e50a3a32150783952d7d099"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8"
+
+  def install
+    product = "wso2is"
+    version = "5.8.0"
+
+    puts "Installing WSO2 Identity Server #{version}..."
+    bin.install "bin/#{product}-#{version}" => "#{product}-#{version}"
+    libexec.install Dir["*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+
+    puts "Installation is completed."
+    puts "\nRun #{product}-#{version} to start WSO2 Identity Server #{version}."
+    puts "\ncheers!!"
+  end
+end
