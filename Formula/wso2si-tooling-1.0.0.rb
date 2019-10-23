dbf346d453b3c457eafe6824eca6308e7583175f
KosalaHerath <sananthanak@gmail.com>
2019-10-23 14:49:59 +0530

    Create formula for wso2si-tooling-1.0.0

---
+++ Formula/wso2si-tooling-1.0.0.rb
@@ -0,0 +1,24 @@
+class Wso2siTooling100 < Formula
+  desc "WSO2 Streaming Integrator Tooling 1.0.0"
+  homepage "https://wso2.com/integration/"
+  url "https://dl.bintray.com/wso2/binary/wso2si-tooling-1.0.0.zip"
+  sha256 "356ce603a245db1e51d62edb3c47c5d7d6649adc3baaa76d633bdd27049a27be"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8"
+
+  def install
+    product = "wso2si-tooling"
+    version = "1.0.0"
+
+    puts "Installing WSO2 Streaming Integrator Tooling #{version}..."
+    bin.install "bin/#{product}-#{version}" => "#{product}-#{version}"
+    libexec.install Dir["*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+
+    puts "Installation is completed."
+    puts "\nRun #{product}-#{version} to start WSO2 Micor Integrator #{version}."
+    puts "\ncheers!!"
+  end
+end
